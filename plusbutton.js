cb12d0627231652e8b2349fe825913ba9ec83f66
Nasiff <nasif_fcb_haque@yahoo.com>
2018-07-17 13:53:29 -0400

    Added a functionality to the add button

---
+++ plusbutton.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+});
+
+function addFile() {
+    $(".plus-button").click(function () {
+        var bookmark = "";
+        $(".plus-button").before(bookmark);
+    });
+}
+
