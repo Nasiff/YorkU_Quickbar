2aca03ef933aa29337fbfde6de867ff073234ca6
Nasiff <nasif_fcb_haque@yahoo.com>
2018-08-05 13:02:03 -0400

    Added Modal Button functionality

--- plusbutton.js
+++ plusbutton.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
-
+    getBookmarkFields()
 });
 
-function addFile() {
-    $(".plus-button").click(function () {
-        var bookmark = "";
-        $(".plus-button").before(bookmark);
-    });
+function getBookmarkFields() {
+    $("#saveBtn").click(function () {
+        var siteName = $('input[name="name"]').val();
+        var siteUrl = $('input[name="url"]').val();
+        addBookmarkFile(siteName, siteUrl);
+    })
+}
+
+function addBookmarkFile(siteName, siteUrl) {
+    $(".plus-button").before(bookmark);
 }
 
